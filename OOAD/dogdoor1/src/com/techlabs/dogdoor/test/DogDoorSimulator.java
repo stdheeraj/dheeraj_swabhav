75e17d13ea4054e25f1408d30f805e1db9709c39
stdheeraj <cdheeraj76@gmail.com>
2018-03-24 10:15:36 +0530

    DogDoor folder added

---
+++ OOAD/dogdoor1/src/com/techlabs/dogdoor/test/DogDoorSimulator.java
@@ -0,0 +1,35 @@
+package com.techlabs.dogdoor.test;
+
+import com.techlabs.dogdoor.BarkRecognizer;
+import com.techlabs.dogdoor.DogDoor;
+import com.techlabs.dogdoor.Remote;
+
+public class DogDoorSimulator {
+
+	public static void main(String[] args) {
+
+		DogDoor door = new DogDoor();
+		BarkRecognizer recognizer = new BarkRecognizer(door);
+		Remote remote = new Remote(door);
+		
+		System.out.println("Fido starts barking.");
+		recognizer.recognize("Woof");
+		
+		System.out.println("\nFido has gone outside...");
+		
+		System.out.println("\nFido's all done...");
+		
+		try{
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e){}
+		
+		System.out.println("... but he's stuck outside!");
+		
+		System.out.println("\nFido starts barking...");
+		recognizer.recognize("Woof");
+
+		System.out.println("\nFido's back inside...");
+		
+	}
+
+}
