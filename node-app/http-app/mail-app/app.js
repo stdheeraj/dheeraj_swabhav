6723643949961caffc64fb50c898549dc9e5f646
stdheeraj <cdheeraj76@gmail.com>
2018-06-28 08:15:33 +0530

    Mail app folder added

---
+++ node-app/http-app/mail-app/app.js
@@ -0,0 +1,28 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'cdheeraj76@gmail.com',
+    pass: 'Application12'
+  },
+  tls: {
+      rejectUnauthorized: false
+  }
+});
+
+var mailOptions = {
+  from: 'cdheeraj76@gmail.com',
+  to: 'kannan@swabhavtechlabs.com',
+  subject: 'Sending Email using Node.js',
+  text: 'Good Morning sir, This mail is send throught node js application.'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+  transporter.close();
+});
